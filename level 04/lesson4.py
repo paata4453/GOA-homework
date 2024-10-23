2e3110a32e5b3882ae1f95c17a5f2863d21730bd
paata4453 <cucqiridzepaata2@gmail.com>
2024-10-23 19:49:26 +0400

    commit

---
+++ level 04/lesson4.py
@@ -0,0 +1,67 @@
+name= "paata"
+last_name="cucqiridze"
+age="13"
+input("name")
+input("last_name")
+input("age")
+print(name +" " +last_name + " "+ "არის" +" "+age)
+
+
+first=input=6
+second=input=3
+
+print(first + second)
+print(first / second)
+print(first * second)
+print(first - second)
+
+
+
+#1user = input"enter your name: "
+#print(1user)
+
+
+
+#math = 20 + "20"
+#2math = math - 10
+#3math = math * 2
+#print = 3math
+
+math= 20 + 20
+math2= math -10
+math3= math * 2
+print(math3)
+
+academy=("your academy")
+school="your school"
+name1="your name"
+last_name1=("your last name")
+age2="your age"
+
+
+input(academy)
+input(school)
+input(name)
+input(last_name1)
+input(age)
+print(academy+school+name+last_name1+age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ level arvici/level arvici.py
@@ -0,0 +1,19 @@
+print(2 > 2)
+print(69 > 2)
+
+print(2 < 69)
+print(2 < 15)
+
+print(2 >= 20)
+print(2 >= 20)
+
+print(2 == 20)
+print(2 == 2)
+
+print(2 >= 20)
+print(2 != 69)
+
+
+grisha= 10
+misha= 20 
+print(misha < grisha)
