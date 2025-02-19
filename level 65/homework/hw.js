0a3ef3f8bee48570195dad245556ac8a2cd374a3
paata4453 <cucqiridzepaata2@gmail.com>
2025-02-20 00:14:21 +0400

    U♥ ♥R♥ ♥b♥e♥s♥t♥ ♥c♥o♥n♥t♥r♥o♥l♥l♥e♥r

---
+++ level 65/homework/hw.js
@@ -0,0 +1,68 @@
+const obj1 = {
+    favColor : "Blue",
+    favDrink : "Not coca cola",
+    favFood : "Pizza",
+    favNumber : 7,
+    favAnimal : "Dog",
+    favCar : "BMW",
+    favSport : "Football",
+    favMovie : "Inception",
+    favMusic : "Rock",
+    favBook : "Harry Potter",
+    favCountry : "Georgia",
+    favCity : "Tbilisi",
+    favGame : "vs code",
+    favLanguage : "JavaScript",
+    favFramework : "React",
+    favLibrary : "React",
+    favDatabase : "MongoDB",
+    favOS : "Windows",
+    favBrowser : "Chrome",
+    favPet : "Dog",
+    favSeason : "Summer",
+    favWeather : "Sunny",
+    favTimeOfDay : "Night",
+    favDayOfWeek : "Saturday",
+    favMonth : "July",
+    favHoliday : "New Year",
+    favFestival : "Tbilisoba",
+    favPlace : "Home",
+    favActivity : "Coding",
+    favHobby : "Coding",
+    favSkill : "Coding",
+    favSubject : "Math",
+    favSchool : "School of life",
+    favUniversity : "MIT",
+    favJob : "Software Engineer",
+    favCompany : "Google",
+    favPosition : "Senior Software Engineer",
+    favSalary : "1000000",
+    favCurrency : "USD",
+    favContinent : "Europe",
+    favCountry : "Georgia",
+    favCity : "Tbilisi",
+    favContinent : "Europe",
+    favPlanet : "Earth",
+    favGalaxy : "Milky Way",
+    favUniverse : "Multiverse",
+    favColor : "Blue",
+    favDrink : "Not coca cola",
+    favFood : "Pizza",
+}
+// ეს copilot-ს დავაწერინე და ვეფხის ტყაოსანი დაწერა დდ;
+console.log(obj1)
+
+
+function Motorcycle(brand, name, color, method){
+    this.brand = brand;
+    this.name = name;
+    this.color = color;
+    this.method = method;
+}
+
+function switchColor(){
+    this.color = "Blue"
+}
+
+const Motorcycle1 = new Motorcycle("Yamaha", "Yamaha motor R9", "Black", switchColor) // არ გეგონოთ რომ chat gpt დავაწერინე უბრალოდ copilot cook-ავს
+console.log(Motorcycle1)
